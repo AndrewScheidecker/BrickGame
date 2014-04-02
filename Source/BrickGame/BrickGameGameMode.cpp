9749f9cbd2f0cb8b39f2d37507086c674271d2fc
Andrew Scheidecker <andrew@scheidecker.net>
2014-04-01 21:12:09 -0400

    Made left-click add a gold brick Made right-click remove a gold brick Use BoxComponents to show where the bricks will be created/destroyed
    Work around plugin/blueprint loading order problems:
    * Add GameMode blueprint
    * Replace C++ GameMode reference to MyCharacter blueprint with a reference from the blueprint GameMode
    * Allows MyCharacter to reference BrickGridComponent class in the BrickGrid plugin, which isn't loaded yet if MyCharacter is loaded with the C++ game code

--- Source/BrickGame/BrickGameGameMode.cpp
+++ Source/BrickGame/BrickGameGameMode.cpp
@@ -7,13 +7,6 @@
 ABrickGameGameMode::ABrickGameGameMode(const class FPostConstructInitializeProperties& PCIP)
 	: Super(PCIP)
 {
-	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FObjectFinder<UBlueprint> PlayerPawnObject(TEXT("/Game/Blueprints/MyCharacter"));
-	if (PlayerPawnObject.Object != NULL)
-	{
-		DefaultPawnClass = (UClass*)PlayerPawnObject.Object->GeneratedClass;
-	}
-
 	// use our custom HUD class
 	HUDClass = ABrickGameHUD::StaticClass();
 }
