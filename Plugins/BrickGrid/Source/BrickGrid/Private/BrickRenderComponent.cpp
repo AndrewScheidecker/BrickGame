11e9ec1f95041aa7c216ea2023f5b98b02bfa574
Andrew Scheidecker <andrew@scheidecker.net>
2016-04-14 15:16:13 -0400

    Fix compile error due to 4.11's addition of const qualifier to GetViewRelevance

--- Plugins/BrickGrid/Source/BrickGrid/Private/BrickRenderComponent.cpp
+++ Plugins/BrickGrid/Source/BrickGrid/Private/BrickRenderComponent.cpp
@@ -279,7 +279,7 @@ public:
 		}
 	}
 
-	virtual FPrimitiveViewRelevance GetViewRelevance(const FSceneView* View) override
+	virtual FPrimitiveViewRelevance GetViewRelevance(const FSceneView* View) const override
 	{
 		FPrimitiveViewRelevance Result;
 		Result.bDrawRelevance = IsShown(View);
